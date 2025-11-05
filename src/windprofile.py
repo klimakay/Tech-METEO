f9a9f7b627c01d6a77e0b9f4e9dff228158be1a0
Kay Rabe von Kühlewein <krvk@gmx.de>
2025-11-05 10:27:54 +0100

    import pandas, defined important constants

--- src/windprofile.py
+++ src/windprofile.py
@@ -4,4 +4,12 @@ All functions and plots are stored and created here for the wind profiles
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+# define constants
+z0=1
+g=9.81
+p0=1013
+R=287.1
+cp=1005
 
