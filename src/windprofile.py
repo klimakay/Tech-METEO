b48ae33e4537f39a26e19bb31d475cd172ac5ed6
Kay Rabe von Kühlewein <krvk@gmx.de>
2025-11-06 16:30:40 +0100

    renamed all variables from data-types to constants if constants. define height z

--- src/windprofile.py
+++ src/windprofile.py
@@ -14,6 +14,9 @@ R=287.1
 cp=1005
 kappa=0.41
 
+# define height
+z=np.linspace(0,1000,10)
+
 # define potential temperature
 data['theta']=data['T']*(data['p']/p0)**(R/cp)
 
@@ -23,34 +26,32 @@ data['v_fluc']=data['v']-np.mean(data['v']
 data['w_fluc']=data['w']-np.mean(data['w'])
 
 # kinematic heat and momentum fluxes
-data['uw']=np.mean(data['u_fluc']*data[w_fluc])
-data['uv']=np.mean(data['u_fluc']*data['v_fluc'])
-data['thetaw']=np.mean(data['theta']*data['w_fluc']))
+uw=np.mean(data['u_fluc']*data['w_fluc'])
+uv=np.mean(data['u_fluc']*data['v_fluc'])
+thetaw=np.mean(data['theta']*data['w_fluc']))
 
 # define shear stress rate and characteristic temperature
-data['u_star']=((np.mean(data['uw']))**2+(np.mean(data['uv']))**2)**(1/4)
-data['theta_star']=-(np.mean(data['thetaw'])/data['u_star'])
+u_star=((np.mean(uw))**2+(np.mean(uv))**2)**(1/4)
+theta_star=-(np.mean(thetaw)/u_star)
 
 # obukhov-length and vertical coordinate zeta
-data['L']=(data['u_star'])**3 /(kappa*g/data['theta']*np.mean(data['thetaw']))
-data['zeta']=data['z']/data['L']
+L=(u_star)**3/(kappa*g/data['theta']*np.mean(thetaw))
+data['zeta']=z/L
 
 # define X and X_0
-data['X']=(1-15*(data['zeta']))**(-1/4)
-data['X_0']=(1-15*(z_0/(data['L'])))**(-1/4)
+X=(1-15*(data['zeta']))**(-1/4)
+X_0=(1-15*(z_0/(L)))**(-1/4)
 
-def u_speed(data, z0, kappa):
+def u_speed(z):
     """
     function which computes the wind speed in direction of u depending on the stability
-    :param data: all data from pandas input
-    :param z0: roughness length
-    :param kappa: von_karman constant
+    :param z: height in meters
     :return: u_speed
     """
     if data['zeta']<=0:
-        u_speed=data['u_star']/kappa*(np.ln(data['z']/z0)-2*np.ln((1+data['X'])/1+data['X_0'])-np.ln((1+data['X']**2)/1+data['X_0']**2)+2*np.arctan2(data['X'])-2*np.arctan2(data['X_0']))
+        u_speed=u_star/kappa*(np.ln(z/z0)-2*np.ln((1+X)/1+X_0)-np.ln((1+X**2)/1+X_0**2)+2*np.arctan2(X)-2*np.arctan2(X_0))
     elif data['zeta']>=0 and data['zeta']<=0.5:
-        u_speed=data['u_star']/kappa*(np.ln(data['z']/z0)+5*(data['z']-z0/data['L']))
+        u_speed=u_star/kappa*(np.ln(z/z0)+5*(z-z0/L))
     return u_speed
 
 
