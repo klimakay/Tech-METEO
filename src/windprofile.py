6814b070157b31f381a59af2d4d6c0b08b85e168
Kay Rabe von Kühlewein <krvk@gmx.de>
2025-12-19 13:39:33 +0100

    define the height for the wind profile

--- src/windprofile.py
+++ src/windprofile.py
@@ -5,6 +5,10 @@ All functions and plots are stored and created here for the wind profiles
 import numpy as np
 import matplotlib.pyplot as plt
 
+# define the height for the wind profile
+z=np.linspace(0,200,50)
+
+
 
 
 
