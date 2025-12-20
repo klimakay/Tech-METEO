49e16d9dfbc9f6a6133fb9922c27afae41c260df
Kay Rabe von Kühlewein <krvk@gmx.de>
2025-12-20 16:37:30 +0100

    define constants for now

--- src/windprofile.py
+++ src/windprofile.py
@@ -5,6 +5,10 @@ All functions and plots are stored and created here for the wind profiles
 import numpy as np
 import matplotlib.pyplot as plt
 
+#define constants for now
+u_A=3.0
+z_A=10
+
 # define dictionaries for roughness in rural and city areas only for considered atmospheric conditions
 m_rural = {"I": 0.37, "III/2": 0.18}
 m_city = {"I": 0.52, "III/2": 0.31}
