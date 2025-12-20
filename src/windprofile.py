0f61eb18a1123043dffa95b3c59c8ce474d388ab
Kay Rabe von Kühlewein <krvk@gmx.de>
2025-12-20 16:19:30 +0100

    turn the stupid lists to real dictionaries

--- src/windprofile.py
+++ src/windprofile.py
@@ -6,11 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # define dictionaries for roughness in rural and city areas only for considered atmospheric conditions
-m_rural=["I":0.37,"III/2":0.18]
-m_city=["I":0.52,"III/2":0.31]
+m_rural = {"I": 0.37, "III/2": 0.18}
+m_city = {"I": 0.52, "III/2": 0.31}
 
 # define the height for the wind profile
 z=np.linspace(0,200,50)
+
+
 
 
 
