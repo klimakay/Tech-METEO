bf0db6f42dbea2e16539ffe612bbec78510d2322
Kay Rabe von Kühlewein <krvk@gmx.de>
2025-12-20 15:06:59 +0100

    define dictionaries for roughness

--- src/windprofile.py
+++ src/windprofile.py
@@ -5,6 +5,10 @@ All functions and plots are stored and created here for the wind profiles
 import numpy as np
 import matplotlib.pyplot as plt
 
+# define dictionaries for roughness in rural and city areas only for considered atmospheric conditions
+m_rural=["I":0.37,"III/2":0.18]
+m_city=["I":0.52,"III/2":0.31]
+
 # define the height for the wind profile
 z=np.linspace(0,200,50)
 
