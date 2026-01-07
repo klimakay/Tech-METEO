0c0e7889564d4e5b80bb17d47a32168e74f76f68
Kay Rabe von Kühlewein <krvk@gmx.de>
2026-01-07 20:57:08 +0100

    einführung name cat in datei namen

--- src/gauß model.py
+++ src/gauß model.py
@@ -63,5 +63,5 @@ for disp in range(len(disp_cat)):
         plt.title(f"Ausbreitungsklasse {cat}")
         plt.xlabel("Entfernung von der Quelle in m")
         plt.ylabel("Emittierte Konzentration in Millionstel")
-        plt.legend()
-    plt.savefig(output_path/f"Ausbreitungskategorie 32 mit verschiedenen Emissionshöhen.png", dpi=300)
+    plt.legend()
+    plt.savefig(output_path/f"Ausbreitungskategorie {name_cat} mit verschiedenen Emissionshöhen.png", dpi=300)
